bef42bd9409be29fbc937c7d215430bc2470ca4c
Kim MyungSub <audtjqxx@naver.com>
2023-10-29 23:25:14 +0900

    테마 버튼 애니메이션 효과 추가

--- scripts/content.js
+++ scripts/content.js
@@ -120,17 +120,32 @@ const VELOG_PREVIEW_TEMPLATE_KEY = 'velog_preview_template';
 
   select()('#toolbar').style.transition = 'none';
   function createModeChangeBtn() {
+    const $toolbar = select()('#toolbar');
     const $modeChangeBtn = create('button');
+    $modeChangeBtn.innerHTML = `
+    <div class="ThemeToggleButton_positional__neE4C"><div class="ThemeToggleButton_svgWrapper__Yr6R3"><div><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path d="M4.069 13H0v-2h4.069c-.041.328-.069.661-.069 1s.028.672.069 1zm3.034-7.312L4.222 2.807 2.808 4.221l2.881 2.881a8.019 8.019 0 0 1 1.414-1.414zm11.209 1.414 2.881-2.881-1.414-1.414-2.881 2.881a8.121 8.121 0 0 1 1.414 1.414zM12 4c.339 0 .672.028 1 .069V0h-2v4.069A8.047 8.047 0 0 1 12 4zm0 16c-.339 0-.672-.028-1-.069V24h2v-4.069A8.047 8.047 0 0 1 12 20zm7.931-9c.041.328.069.661.069 1s-.028.672-.069 1H24v-2h-4.069zm-3.033 7.312 2.88 2.88 1.415-1.414-2.88-2.88a8.127 8.127 0 0 1-1.415 1.414zm-11.21-1.415-2.88 2.88 1.414 1.414 2.88-2.88a8.053 8.053 0 0 1-1.414-1.414zM12 6a6 6 0 1 0 0 12 6 6 0 0 0 0-12z"></path></svg></div></div></div>
+    `;
+    $modeChangeBtn.classList.add(
+      'ThemeToggleButton_block__V9gIU',
+      'change-mode-btn',
+      'ixiTKc',
+    );
     const templateWrapper_modeChange = create('div');
+
+    const $toolbarAreaDiv = create('div');
+    $toolbarAreaDiv.classList.add('sc-kHOZwM', 'jvqdVW');
+
     templateWrapper_modeChange.style.cssText = `
-      position: absolute;
-      top : 0;
-      left : 0;
       width:auto;
       height: auto;
     `;
-    $modeChangeBtn.innerText = '모드체인지';
-    append(body, append(templateWrapper_modeChange, $modeChangeBtn));
+
+    const addToolbar = [
+      $toolbarAreaDiv,
+      append(templateWrapper_modeChange, $modeChangeBtn),
+    ];
+
+    append($toolbar, addToolbar);
     $modeChangeBtn.addEventListener('click', modeChangeHandler);
   }
 
@@ -138,15 +153,40 @@ const VELOG_PREVIEW_TEMPLATE_KEY = 'velog_preview_template';
     const $body = select()('body');
     const currentMode = $body.dataset['theme'];
     const $codeMirror = select()('.CodeMirror');
+    const $svg = select()('.ThemeToggleButton_svgWrapper__Yr6R3 svg');
 
+    console.log($svg);
     if (currentMode === 'dark') {
       $body.dataset['theme'] = 'light';
       $codeMirror.classList.remove('cm-s-one-dark');
       $codeMirror.classList.add('cm-s-one-light');
+      $svg.innerHTML = `<path d="M4.069 13H0v-2h4.069c-.041.328-.069.661-.069 1s.028.672.069 1zm3.034-7.312L4.222 2.807 2.808 4.221l2.881 2.881a8.019 8.019 0 0 1 1.414-1.414zm11.209 1.414 2.881-2.881-1.414-1.414-2.881 2.881a8.121 8.121 0 0 1 1.414 1.414zM12 4c.339 0 .672.028 1 .069V0h-2v4.069A8.047 8.047 0 0 1 12 4zm0 16c-.339 0-.672-.028-1-.069V24h2v-4.069A8.047 8.047 0 0 1 12 20zm7.931-9c.041.328.069.661.069 1s-.028.672-.069 1H24v-2h-4.069zm-3.033 7.312 2.88 2.88 1.415-1.414-2.88-2.88a8.127 8.127 0 0 1-1.415 1.414zm-11.21-1.415-2.88 2.88 1.414 1.414 2.88-2.88a8.053 8.053 0 0 1-1.414-1.414zM12 6a6 6 0 1 0 0 12 6 6 0 0 0 0-12z"></path>`;
+      $svg.parentElement.animate(
+        {
+          transform: ['none', 'scale(1) rotate(90deg) translateZ(0px)'],
+        },
+        {
+          duration: 500,
+          fill: 'forwards',
+          easing: 'ease',
+        },
+      );
     } else if (currentMode === 'light') {
       $body.dataset['theme'] = 'dark';
       $codeMirror.classList.remove('cm-s-one-light');
       $codeMirror.classList.add('cm-s-one-dark');
+      $svg.parentElement.style.transform = 'none';
+      $svg.innerHTML = `<path fill-rule="evenodd" d="M20.614 14.614A9 9 0 0 1 9.385 3.386a9 9 0 1 0 11.229 11.229Z" clip-rule="evenodd"></path>`;
+      $svg.parentElement.animate(
+        {
+          transform: ['scale(1) rotate(90deg) translateZ(0px)', 'none'],
+        },
+        {
+          duration: 500,
+          fill: 'forwards',
+          easing: 'ease',
+        },
+      );
     }
   }
 
